63ec044aabc7bbdd436636fe5c507311ba0ec729
cxm <cxmmxc@126.com>
2018-11-12 18:05:53 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/android/coolweather/cxm/coolweather/db/County.java
@@ -0,0 +1,42 @@
+package android.coolweather.cxm.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int citId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCitId() {
+        return citId;
+    }
+
+    public void setCitId(int citId) {
+        this.citId = citId;
+    }
+}
